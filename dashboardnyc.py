a6cd8a96ca94c13afb9f28b2d5e0af295c8d2eab
beauvantrojen <beau@vantrojen.com>
2025-03-23 14:12:16 +0100

    Added a limit

--- dashboardnyc.py
+++ dashboardnyc.py
@@ -483,7 +483,8 @@ elif page == "Delay Analysis":
                     AND f.year = w.year 
                     AND f.month = w.month 
                     AND f.day = w.day
-                WHERE f.arr_delay IS NOT NULL;
+                WHERE f.arr_delay IS NOT NULL
+                LIMIT 10000;
             """
             df = pd.read_sql_query(query, conn)
             return df
