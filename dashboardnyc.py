cd782e4b3b1386c42a157bf8fcea1ce03ae6c74d
beauvantrojen <beau@vantrojen.com>
2025-03-23 10:44:06 +0100

    Update dashboardnyc.py
    maplibre error fixed

--- dashboardnyc.py
+++ dashboardnyc.py
@@ -438,7 +438,7 @@ elif page == "Flight Route Statistics":
                     zoom=3,
                     title="Airport Locations",
             )
-            fig_map.update_layout(maplibre_style="open-street-map")
+            
             st.plotly_chart(fig_map, use_container_width=True)
 
             query_top_destinations = """
